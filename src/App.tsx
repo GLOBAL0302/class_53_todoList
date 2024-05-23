a982f4a6cbf73f86495991f2ec06ea88834d08a2
Abdashimov Kubanychbek <abdashimov99@mail.ru>
2024-05-24 03:32:43 +0400

    git init

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+  const [tasks, setTasks] = React.useState([
+    {title: "buy milk", complete: false},
+    {title: "walk with dog", complete: false},
+    {title: "do homework", complete: false}
+  ])
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
